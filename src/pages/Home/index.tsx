5cfd9a9d41d4bfa7b566b2dcbefd41715a8ad6f8
Andre Sena <89034251+gitsena@users.noreply.github.com>
2022-09-19 20:07:14 -0300

    Feat/intro (#6)
    * feat criação da introdução da home com os beneficios de cada usuario
    
    * fix correção csso

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,14 @@ import Header from '../../components/Header';
 import Hero from './components/Hero';
 import FooterHome from './components/FooterHome';
 import Footer from '../../components/Footer';
+import Intro from './components/Intro';
 
 export function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <Intro />
       <FooterHome />
       <Footer />
     </>
