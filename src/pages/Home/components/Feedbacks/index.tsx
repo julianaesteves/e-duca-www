6c509b0cd582edd61c314a126361969eaf13b895
Campos <rodrigo.r.campos@accenture.com>
2022-10-07 23:39:43 -0300

    feat: add initial feedback cards

--- src/pages/Home/components/Feedbacks/index.tsx
+++ src/pages/Home/components/Feedbacks/index.tsx
@@ -1,38 +1,55 @@
-import style from './feedbacks.module.scss'
-//import feedbackBackground from '../../../../assets/img/image01.png';
-import React, { Component } from "react";
-import Slider from "react-slick";
+import style from './feedbacks.module.scss';
 
-export default class Feedbacks extends Component {
-    render() {
-      const settings = {
-        className: "center",
-        centerMode: true,
-        infinite: true,
-        centerPadding: "60px",
-        slidesToShow: 3,
-        speed: 500
-      };
-      return (
-        
-        <div>
-          <h2 className={style.title}>Feedbacks</h2>
-          <Slider {...settings}>
-            <div>
-              <div className={style.cards}>
-                <h2>Marcos souza</h2>
+export default function Feedbacks() {
 
-              </div>
+  return (
+    <section className={style.feedbacks}>
+      <h2 className={style.title}>Feedbacks</h2>
+      <div className={style.cards}>
+        <div className={style.card}>
+          <aside className={style.titleCard}>
+            <div className={style.circle}></div>
+            <div className={style.textTitle}>
+              <h2>Roberto Silva</h2>
+              <h3>Aluno</h3>
             </div>
-            <div>
-              <h3>2</h3>
-            </div>
-            <div>
-              <h3>3</h3>
-            </div>
-            
-          </Slider>
+          </aside>
+
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry
+            s standard dummy text ever since the 1500s, when an
+            unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
         </div>
-      );
-    }
-  }
+        <div className={style.card}>
+          <aside className={style.titleCard}>
+            <div className={style.circle}></div>
+            <div className={style.textTitle}>
+              <h2>Marcos Souza</h2>
+              <h3>Aluno</h3>
+            </div>
+          </aside>
+
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry
+            s standard dummy text ever since the 1500s, when an
+            unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+        <div className={style.card}>
+          <aside className={style.titleCard}>
+            <div className={style.circle}></div>
+            <div className={style.textTitle}>
+              <h2>Pablo Lucas</h2>
+              <h3>Aluno</h3>
+            </div>
+          </aside>
+
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry
+            s standard dummy text ever since the 1500s, when an
+            unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </div>
+      </div>
+    </section>
+
+  )
+}
