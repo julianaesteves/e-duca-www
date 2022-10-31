d9b0a3af7dc4e79d8fadcb1a11b748b4441285cf
Andre Sena <89034251+gitsena@users.noreply.github.com>
2022-10-31 20:19:56 -0300

    feat: componente sidebar (#15)

--- src/pages/components/Sidebar/index.tsx
+++ src/pages/components/Sidebar/index.tsx
@@ -1,8 +1,25 @@
-import style from './sidebar.module.scss'
+import style from './sidebar.module.scss';
+import Logo from '../../../assets/img/logo.png';
+import { Perfil } from './Perfil';
+import Menu from './Menu';
+import perfil from '../../../assets/img/perfilTeacher.svg';
 
-export function Sidebar() {
-    return (
-        <>
-        </>
-    )
+export default function Sidebar() {
+  return (
+    <>
+      <div className={style.container}>
+        <img className={style.logo} src={Logo} alt="Logotipo da e-duca" />
+        <Perfil
+          photo={perfil}
+          name="Claudio Roberto"
+          userType="Professor"
+          labOne="Tempo de carreira:"
+          carrerTime="10 anos"
+          labtwo="Área de atuação:"
+          occupation="Matemática"
+        />
+        <Menu />
+      </div>
+    </>
+  );
 }
