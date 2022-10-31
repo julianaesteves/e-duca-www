d8891558f76bd1ee55b782f01d0cc6a7f7ab0249
Campos <rodrigo.r.campos@accenture.com>
2022-10-31 19:41:48 -0300

    fix: Página de erro

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -3,15 +3,17 @@ import FooterHome from "../Home/components/FooterHome";
 import Footer from "../../components/Footer";
 import style from './error.module.scss';
 import imageError from "../../assets/img/errorImg.png";
+import { useNavigate } from 'react-router-dom';
 
 export default function Error(){
+    const navigate = useNavigate();
     return(
         <>
         <Header/>
         <div className={style.divError} >
         <img src={imageError} alt="Imagem de erro" />
         <p>Ops! Parece que a página que você está procurando não existe.</p>
-        <button >Voltar para a página inicial</button>
+        <button onClick={() => navigate(-1)} >Voltar para a página inicial</button>
         </div>
         <FooterHome/>
         <Footer/>
