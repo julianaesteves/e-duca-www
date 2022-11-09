3d2013be8e7ea2c8e594ceaf3680e1f2ca3ca0b5
Campos <rodrigo.r.campos@accenture.com>
2022-11-09 10:43:25 -0300

    fix/videoClass:formatacao

--- src/components/VideoClass/index.tsx
+++ src/components/VideoClass/index.tsx
@@ -1,15 +1,20 @@
-import style from './videoClass.module.scss';
+import style from './videoClass.module.scss'
 
-type Props={
-   video:string
+type Props = {
+  video: string
 }
 
-export const VideoClass=({video}:Props)=>{
-    return(
-<div className={style.container}>
-<iframe src={video} title="YouTube video player" frameBorder="0" 
-allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
-gyroscope; picture-in-picture" allowFullScreen></iframe>
-</div>
-    )
+export const VideoClass = ({ video }: Props) => {
+  return (
+    <div className={style.container}>
+      <iframe
+        src={video}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  )
 }
