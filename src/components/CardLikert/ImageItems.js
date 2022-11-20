e616ee66009e4f29b0b9675799592ee2f0412a01
Andre Sena <89034251+gitsena@users.noreply.github.com>
2022-11-20 16:26:28 -0300

    feat componente likert, correções (#41)

---
+++ src/components/CardLikert/ImageItems.js
@@ -0,0 +1,27 @@
+export const ImageItems = [
+  {
+    id: 1,
+    src: "../../assets/img/search.svg",
+    text: 'Não gostei'
+  },
+  {
+    id: 2,
+    src: "../../assets/img/search.svg",
+    text: 'Poderia melhorar'
+  },
+  {
+    id: 3,
+    src: "../../assets/img/search.svg",
+    text: 'Não sei'
+  },
+  {
+    id: 4,
+    src: "../../assets/img/search.svg",
+    text: 'Gostei'
+  },
+  {
+    id: 5,
+    src: "../../assets/img/search.svg",
+    text: 'Amei!'
+  }
+]
